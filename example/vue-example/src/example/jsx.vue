b2c4aa8c295804ba5ceba87847c7af702e6898f1
lizongwen <93700983@qq.com>
2018-03-05 18:02:07 +0800

    enter

---
+++ example/vue-example/src/example/jsx.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      test: 123
+    };
+  },
+  render (h) {
+    return (
+      <h1>
+        <span>Hello</span> render 函数
+      </h1>
+    )
+  }
+};
+</script>
+
+<style>
+
+</style>
