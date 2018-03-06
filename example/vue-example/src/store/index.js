5d6cf2350a4856decc1f5198dff5c950dc1170f2
lizongwen <93700983@qq.com>
2018-03-06 16:53:20 +0800

    enter

---
+++ example/vue-example/src/store/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import module_1 from './modules/module_1'
+import module_2 from './modules/module_2'
+// import products from './modules/products'
+// import createLogger from '../../../src/plugins/logger'
+
+Vue.use(Vuex)
+
+const debug = process.env.NODE_ENV !== 'production'
+
+export default new Vuex.Store({
+	modules: {
+		module_1,
+		module_2
+		// cart,
+		// products
+	},
+	//strict: debug,
+	//plugins: debug ? [createLogger()] : []
+})
